883d9d05e957ef84f7095c6eee14496e20fd36a9
Varun Sharma <coolvarunsharma117@gmail.com>
2020-12-23 14:36:13 +0530

    Price added(Demo) #Varun_Sharma

---
+++ app/src/main/java/com/abhi/ourvedic/Admin_Panel_Login.java
@@ -0,0 +1,14 @@
+package com.abhi.ourvedic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Admin_Panel_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin__panel__login);
+    }
+}
