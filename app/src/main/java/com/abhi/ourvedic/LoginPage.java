0a4f8344039e9c76cae7196546d62fb93fe53afe
Varun Sharma <coolvarunsharma117@gmail.com>
2020-12-26 13:47:28 +0530

    ERROR!! in list adapter. Commit might be rollback #Varun_Sharma

--- app/src/main/java/com/abhi/ourvedic/LoginPage.java
+++ app/src/main/java/com/abhi/ourvedic/LoginPage.java
@@ -15,6 +15,15 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -29,6 +38,11 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+
 public class LoginPage extends AppCompatActivity {
 
     ProgressDialog progressDialog;
@@ -39,6 +53,9 @@ public class LoginPage extends AppCompatActivity {
     private static final String EMAIL = "email";
     private TextView forgetText;
     TextView login_as_admin;
+    TextView facebook_sign_in_button;
+    private CallbackManager callbackManager;
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +79,7 @@ public class LoginPage extends AppCompatActivity {
         forgetText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginPage.this,ForgetPassword.class));
+                startActivity(new Intent(LoginPage.this, ForgetPassword.class));
             }
         });
         signInButton.setOnClickListener(new View.OnClickListener() {
@@ -82,6 +99,39 @@ public class LoginPage extends AppCompatActivity {
                 .build();
         // Build a GoogleSignInClient with the options specified by gso.
         mgoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+        callbackManager = CallbackManager.Factory.create();
+
+        facebook_sign_in_button = findViewById(R.id.facebook_sign_in_button);
+        facebook_sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginButton.performClick();
+            }
+        });
+
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+
     }
 
 
@@ -116,7 +166,7 @@ public class LoginPage extends AppCompatActivity {
                                 // Sign in success, update UI with the signed-in user's information
                                 SharedPreferences sh = getSharedPreferences("email", Context.MODE_PRIVATE);
                                 SharedPreferences.Editor edit = sh.edit();
-                                edit.putString("email",email);
+                                edit.putString("email", email);
                                 edit.commit();
                                 Log.d(TAG, "signInWithEmail:success");
                                 gotonext();
@@ -148,6 +198,7 @@ public class LoginPage extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data); //for facebook login
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == 1001) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -183,6 +234,57 @@ public class LoginPage extends AppCompatActivity {
                         // ...
                     }
                 });
+
     }
 
+    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+        @Override
+        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken) {
+            if (currentAccessToken != null) {
+                Intent i2 = new Intent(LoginPage.this, MainActivity.class);
+                startActivity(i2);
+            } else {
+                loadUserProfile(currentAccessToken);
+            }
+        }
+    };
+
+    public void loadUserProfile(AccessToken newAccessToken) {
+        GraphRequest request = GraphRequest.newMeRequest(newAccessToken, new GraphRequest.GraphJSONObjectCallback() {
+            @Override
+            public void onCompleted(JSONObject object, GraphResponse response) {
+
+                Toast.makeText(LoginPage.this, "Two",Toast.LENGTH_SHORT).show();
+
+                //for abhishek
+                    /*
+                try {
+
+                    first_name = object.getString("first_name");
+                    last_name = object.getString("last_name");
+                    email = object.getString("email");
+                    id = object.getString("id");
+                    String img_url = "https://graph.facebook.com/"+ id + "/picture?type=normal";
+
+
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    i.putExtra("name", first_name + " " + last_name);
+                    i.putExtra("email", email);
+                    i.putExtra("img_url", img_url);
+                    Log.v("name", first_name + last_name);
+                    Log.v("email", email);
+                    startActivity(i);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                */
+            }
+        });
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "first_name,last_name,email,id");
+        request.setParameters(parameters);
+        request.executeAsync();
+
+    }
 }
