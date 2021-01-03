4b5e36784f8bb1ba8ec85f94632ab50281cffabd
Varun Sharma <coolvarunsharma117@gmail.com>
2021-01-03 20:21:11 +0530

    New Logo added! #Varun_Sharma

--- app/src/main/java/com/abhi/ourvedic/ui/booking/BookingFragment.java
+++ app/src/main/java/com/abhi/ourvedic/ui/booking/BookingFragment.java
@@ -1,14 +1,17 @@
 package com.abhi.ourvedic.ui.booking;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.abhi.ourvedic.BookDharamshala;
 import com.abhi.ourvedic.R;
 
 public class BookingFragment extends Fragment {
@@ -20,6 +23,14 @@ public class BookingFragment extends Fragment {
         bookingViewModel =
                 ViewModelProviders.of(this).get(BookingViewModel.class);
         View root = inflater.inflate(R.layout.fragment_booking, container, false);
+
+        TextView tv_Dharamshala = root.findViewById(R.id.tv_Dharamshala);
+        tv_Dharamshala.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(), BookDharamshala.class));
+            }
+        });
 
         return root;
     }
